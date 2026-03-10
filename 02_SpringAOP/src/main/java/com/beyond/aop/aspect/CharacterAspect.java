da6285bbffd695693aab65adccc24ad4873daf4e
jin605 <jinddd3@gmail.com>
2026-03-10 17:30:53 +0900

    study : aop 실습

---
+++ 02_SpringAOP/src/main/java/com/beyond/aop/aspect/CharacterAspect.java
@@ -0,0 +1,76 @@
+package com.beyond.aop.aspect;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CharacterAspect {
+    /*
+        포인트 커트 표현식
+            - 스프링 AOP에서 포인트 커트는 AspectJ의 포인트 커트 표현식을 이용해서 정의한다.
+            - execution([접근제한자] 리턴타입 클래스이름.메소드이름([파라미터, ...]))
+                - 메소드 실행에 대한 표현식이다.
+                - * : 모든 값을 표현한다.
+                - .. : 0개 이상을 의미한다.
+            - args()
+                - 대상 메소드에 전달되는 매개값을 어드바이스에 전달하기 위한 표현식이다.
+
+     */
+
+    @Pointcut("execution(* com.beyond.aop.character.Character.quest(..)) && args(questName)")
+    public void questPointcut(String questName) {
+
+    }
+
+    // @Before("execution(* com.beyond.aop.character.Character.quest(..))")
+    // @Before("questPointcut()")
+    @Before(value = "questPointcut(questName)", argNames = "questName")
+    public void beforeQuest(String questName) {
+
+        // 퀘스트를 수행하기 전에 필요한 작업들을 작성한다.
+        System.out.printf("%s 퀘스트 준비 중..\n",questName);
+    }
+
+    // @After("execution(* com.beyond.aop.character.Character.quest(..))")
+    // @After("questPointcut()")
+    // @After(value = "questPointcut(questName)", argNames = "questName")
+    public void afterQuest(String questName) {
+
+        // 퀘스트를 수행한 결과에 상관없이 필요한 작업들을 작성한다.
+        System.out.printf("%s 퀘스트 수행 완료..\n",questName);
+    }
+
+    // @AfterReturning(value = "questPointcut(questName)", argNames = "questName")
+    @AfterReturning(
+            value = "questPointcut(questName)",
+            returning = "result",
+            argNames = "questName, result"
+    )
+    public void success(String questName, String result) {
+        // 쿼스트가 정상적으로 완료된 후 필요한 작업들을 작성한다.
+        System.out.printf("result : %s",result);
+        System.out.printf("%s 퀘스트 수행 완료..\n", questName);
+
+    }
+
+    //@AfterThrowing(value = "questPointcut(questName)", argNames = "questName")
+    @AfterThrowing(
+            value = "questPointcut(questName)",
+            throwing = "exception",
+            argNames = "questName, exception"
+    )
+    public void fail(String questName, Exception exception) {
+
+        // 퀘스트 수행 중 에러가 밸생했을 때 필요한 작업들을 작성한다.
+
+        System.out.printf("message : %s\n",exception.getMessage());
+        System.out.printf("%s 퀘스트 수행 중 에러가 발생했습니다..\n", questName);
+
+    }
+
+}
---
+++ 02_SpringAOP/src/test/java/com/beyond/aop/character/CharacterTest.java
@@ -0,0 +1,45 @@
+package com.beyond.aop.character;
+
+import com.beyond.aop.config.RootConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = RootConfig.class)
+class CharacterTest {
+
+    @Autowired
+    private Character character;
+
+    @Test
+    @Disabled
+    @DisplayName("실행 환경 테스트")
+    void nothing() {
+    }
+
+    @Test
+    @DisplayName("Character 객체 생성 테스트")
+    void create() {
+
+        // System.out.println(character);
+
+        assertThat(character).isNotNull();
+        assertThat(character.getWeapon()).isNotNull();
+    }
+
+    @Test
+    void questTest() {
+
+        character.quest("도토리 줍기");
+        // 프록시 객체 확인
+        // System.out.println(character.getClass());
+    }
+}
